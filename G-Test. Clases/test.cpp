430b8e8015d2f71196cbd6164de34c816295b457
rostikzaiats <rostislavzaac2@gmail.com>
2025-03-21 11:10:07 +0200

    Some realisatoin

--- G-Test. Clases/test.cpp
+++ G-Test. Clases/test.cpp
@@ -1,5 +1,5 @@
 #include "pch.h"
-#include"..\Program V-15\Clases.h"
+#include"..\Program V-15\Wine.h"
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
---
+++ Program V-15/Wine.cpp
@@ -0,0 +1,11 @@
+#include "Wine.h"
+
+Wine::Wine(): w_name(""), w_color(""), w_swetness(""), w_strength(0.0){}
+
+Wine::Wine(const string& name, const string& color, const string& swetness, double strength)
+	: w_name(name), w_color(color), w_swetness(swetness), w_strength(strength){}
+
+Wine::Wine(const Wine& P) : w_name(P.w_name), w_color(P.w_color), w_swetness(P.w_swetness), 
+w_strength(P.w_strength){}
+
+Wine::~Wine(){}
