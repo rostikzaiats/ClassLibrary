86ac72ddba210335e98ee54025b4065eb82c292b
2121Ret <2131ret@gmail.com>
2025-03-21 11:13:28 +0200

    pakagedWine code

--- Program V-15/packagedWine.h
+++ Program V-15/packagedWine.h
@@ -1,2 +1,18 @@
 #pragma once
+#include "Wine.h"
+#include <string>
 
+Class PackagedWine : public Wine
+{
+private:
+	double volume;
+	string container;
+
+public:
+	PackagedWine();
+	PackagedWine(double volume, string container);
+	PackagedWine(const PackagedWine& P);
+	virtual ~PackagedWine();
+
+	
+}
