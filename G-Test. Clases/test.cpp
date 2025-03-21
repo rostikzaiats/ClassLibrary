89a2d3a9367ced9c60c3f6424a101bf7727bca11
Youngking147 <turchynpavloua@gmail.com>
2025-03-21 23:58:19 +0200

    Удосконалив main

--- G-Test. Clases/test.cpp
+++ G-Test. Clases/test.cpp
@@ -1,6 +1,1 @@
-#include "pch.h"
-#include"..\Program V-15\Wine.h"
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-}
+//#include "pch.h"
--- Program/Main.cpp
+++ Program/Main.cpp
@@ -1,12 +1,54 @@
 #include"..\Program V-15\Wine.h"
 #include <iostream>
-
+#include"..\Program V-15\Wine.h"
+#include"..\Program V-15\WineBox.h"
+#include"..\Program V-15\packagedWine.h"
 
 
 int main()
 {
     std::cout << "Hello World!\n";
     
+    Wine wine1("Chardonnay", "White", "Dry", 12.0);
+    PackagedWine packagedWine1("Merlot", "Red", "Dry", 14.0, 1.5);
+    PackagedWine packagedWine2("Cabernet", "Red", "Sweet", 13.5, 2.0);
+
+    cout << "Wine Information:" << endl;
+    wine1.printOn(cout);
+    cout << endl;
+
+    cout << "Packaged Wine Information:" << endl;
+    packagedWine1.printOn(cout);
+    cout << endl;
+
+    packagedWine2.printOn(cout);
+    cout << endl;
+
+   
+    packagedWine1.changePackage();
+    packagedWine1.printOn(cout);
+    cout << endl;
+
+    WineBox wineBox(0, 5); 
+    wineBox.addW("Chardonnay", "White", "Dry", 12.0, 1.0);
+    wineBox.addW("Pinot Noir", "Red", "Dry", 13.0, 1.5);
+    wineBox.addW("Sauvignon Blanc", "White", "Sweet", 11.5, 0.75);
+    wineBox.addW("Merlot", "Red", "Dry", 14.0, 2.0);
+
+    cout << "All wines in the box:" << endl;
+    wineBox.printW();
+    cout << endl;
+
+   
+    string color = "Red";
+    double totalVolume = wineBox.totalVolumeByColor(color);
+    cout << "Total volume of " << color << " wines: " << totalVolume << " liters" << endl;
+
+    wineBox.loadFromFile("Wine.txt");  
+
+    wineBox.sortByStrenght();
+    cout << "Wines sorted by strength:" << endl;
+    wineBox.printW();
    
 }
 
