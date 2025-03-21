a1cc863db10a6b422441a316c4489ec0777a9d40
Youngking147 <turchynpavloua@gmail.com>
2025-03-21 10:45:22 +0200

    add constructor

---
+++ Program V-15/Wine.h
@@ -0,0 +1,32 @@
+#pragma once
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Wine
+{
+protected: 
+	string w_name;
+	string w_color;
+	string w_swetness;
+	double w_strength;
+private:
+	
+public:
+	Wine();
+	Wine(const string& name, const string& color, const string& swetness, double strength);
+	Wine(const Wine& P);
+	~Wine();
+
+
+
+};
+
+Wine::Wine()
+{
+}
+
+Wine::~Wine()
+{
+}
