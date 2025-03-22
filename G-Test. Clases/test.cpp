901a1ea146b7f53c689d76566db73034d9968fee
Youngking147 <turchynpavloua@gmail.com>
2025-03-22 13:48:31 +0200

    .

--- G-Test. Clases/test.cpp
+++ G-Test. Clases/test.cpp
@@ -2,7 +2,72 @@
 #include"..\Program V-15\Wine.h"
 #include"..\Program V-15\packagedWine.h"
 #include"..\Program V-15\WineBox.h"
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
